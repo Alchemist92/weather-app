ad8d8513aecf60c4f4e4a709306cf0a2a25999e7
Alchemist92 <ngenetola@icloud.com>
2023-01-09 21:02:00 +0100

    content type

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -21,27 +21,7 @@ class CityController extends Controller
                 
                 $data = json_decode($res, true);
 
-               
-                function artoxml($arr, $i=1,$flag=false){
-                    $sp = "";
-                    for($j=0;$j<=$i;$j++){
-                        $sp.="";
-                     }
-                    foreach($arr as $key=>$val){
-                        echo "$sp&lt;".$key."&gt;";
-                        if($i==1) echo "\n";
-                        if(is_array($val)){
-                            if(!$flag){echo"\n";}
-                            artoxml($val,$i+5);
-                            echo "$sp&lt;/".$key."&gt;\n";
-                        }else{
-                              echo "$val"."&lt;/".$key."&gt;\n";
-                         }
-                    }
-                
-                }
-                
-                artoxml(array($data), 1);
+                return response()->xml(['data' => array($data)])->header('Content-Type', 'text/xml');               
             }              
         } catch(Exception $e){
             if($e){
